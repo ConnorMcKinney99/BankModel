6162c8fd88411339d98723c123283fb51e985347
indy501 <56883585+indy501@users.noreply.github.com>
2020-02-06 11:50:19 -0500

    Added objects to the Main class

--- InClass_2020_02_06/src/main/Main.java
+++ InClass_2020_02_06/src/main/Main.java
@@ -1,9 +1,18 @@
 package main;
 
+import bank.Bank;
+import bank.BrickAndMortar;
+import bank.CafeBranch;
+import bank.Online;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Bank fifthThird = new Bank();
+		fifthThird.setCompanyName("Fifth Third");
+		BrickAndMortar brick = new BrickAndMortar();
+		Online site = new Online();
+		CafeBranch coffee = new CafeBranch();
 
 	}
 
